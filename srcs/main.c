1faf0775afa3d26512b50737e8fff0472221a8cf
Pacome BERNIER <pbernier@e3r5p13.42.fr>
2020-01-13 18:50:21 +0100

    [pbernier] - shufle cube is done

--- srcs/main.c
+++ srcs/main.c
@@ -20,7 +20,7 @@ int		main(int argc, char **argv)
 	if (argc != 2 || !(arg_number = arg_count(argv[1])))
 		return (printf("error\n"));
 	cube = init_tab();
-	display(cube, NONE, NULL);
+	//display(cube, NONE, NULL);
 	instructions(cube, arg_number, argv[1]);
 	return (0);
 }
@@ -89,69 +89,45 @@ void	read_instruction(char ***cube, int arg_count, t_move *instruction)
 	while (count < arg_count)
 	{
 		turn = instruction_turn(cube, instruction[count].side);
-		//si reverse changer turn
 		turn_side(turn);
-		if (instruction[count++].mod == mod_twice)
+		if (instruction[count].mod != mod_null)
 			turn_side(turn);
-		printf("[%c][%c][%c]\n", cube[instruction[count - 1].side][0][0], cube[instruction[count - 1].side][0][1], cube[instruction[count - 1].side][0][2]);
-		printf("[%c][%c][%c]\n", cube[instruction[count - 1].side][1][0], cube[instruction[count - 1].side][1][1], cube[instruction[count - 1].side][1][2]);
-		printf("[%c][%c][%c]\n\n", cube[instruction[count - 1].side][2][0], cube[instruction[count - 1].side][2][1], cube[instruction[count - 1].side][2][2]);
+		if (instruction[count].mod == mode_reverse)
+			turn_side(turn);
+		++count;
 	}
 }
 
-# define L_LEFT		((int[3][2]){{0, 0}, {1, 0}, {2, 0}})
-# define L_UP			((int[3][2]){{0, 0}, {0, 1}, {0, 2}})
-# define L_RIGHT	((int[3][2]){{0, 2}, {1, 2}, {2, 2}})
-# define L_DOWN		((int[3][2]){{2, 0}, {2, 1}, {2, 2}})
-
-
-void	init_line(char ***cube, char *line[3], t_side side, int coor[3][2])
+void	init_line(char ***cube, char *data[3], t_line line)
 {
-	line[0] = (&cube[side][coor[0][0]][coor[0][1]]);
-	line[1] = (&cube[side][coor[1][0]][coor[1][1]]);
-	line[2] = (&cube[side][coor[2][0]][coor[2][1]]);
+	data[0] = (&cube[line.side][line.coo[0][0]][line.coo[0][1]]);
+	data[1] = (&cube[line.side][line.coo[1][0]][line.coo[1][1]]);
+	data[2] = (&cube[line.side][line.coo[2][0]][line.coo[2][1]]);
 }
 
-t_turn	init_turn(char ***cube, t_side side[4], int line[4][3][2])
+t_turn	init_turn(char ***cube, t_line line[4])
 {
-	t_turn	turn;
+	t_turn		turn;
 
-	init_line(cube, turn.right, side[0], line[0]);
-	init_line(cube, turn.down, side[1], line[1]);
-	init_line(cube, turn.left, side[2], line[2]);
-	init_line(cube, turn.up, side[3], line[3]);
-	return (turn)
+	init_line(cube, turn.right, line[0]);
+	init_line(cube, turn.down, line[1]);
+	init_line(cube, turn.left, line[2]);
+	init_line(cube, turn.up, line[3]);
+	return (turn);
 }
 
 t_turn	instruction_turn(char ***cube, t_side side)
 {
 	t_turn	turn;
 
-	if (side == side_front)
-		init_turn(cube,
-			((t_side[4]){side_right, side_down, side_left, side_up}),
-			((line[4][3][2]){L_LEFT, L_UP, L_RIGHT, L_DOWN});
-	else if (side == side_right)
-		init_turn(cube,
-			((t_side[4]){side_back, side_down, side_front, side_up}),
-			((line[4][3][2]){L_LEFT, L_RIGHT, L_RIGHT, L_RIGHT});
-	else if (side == side_up)
-		init_turn(cube,
-			((t_side[4]){side_right, side_front, side_lefr, side_back}),
-			((line[4][3][2]){L_UP, L_UP, L_UP, L_UP});
-
+	turn = init_turn(cube, AROUND(side));
 	turn.front = cube[side];
-	(void)side;
-	(void)cube;
 	return (turn);
 }
 
 void	turn_side(t_turn turn)
 {
 	char	swap;
-
-	if (!turn.front)
-		return ;
 
 	swap = turn.front[0][0];
 	turn.front[0][0] = turn.front[2][0];
@@ -165,16 +141,11 @@ void	turn_side(t_turn turn)
 	turn.front[2][1] = turn.front[1][2];
 	turn.front[1][2] = swap;
 
-	//printf("%c %c %c\n", *turn.right[0], *turn.right[1], *turn.right[2]);
-	//printf("{%p}\n", &(*turn.down[0]));
-
-
 	swap = *(turn.down[0]);
 	*(turn.down[0]) = *(turn.right[2]);
 	*(turn.right[2]) = *(turn.up[2]);
 	*(turn.up[2]) = *(turn.left[0]);
 	*(turn.left[0]) = swap;
-
 
 	swap = *(turn.down[1]);
 	*(turn.down[1]) = *(turn.right[1]);
@@ -188,7 +159,5 @@ void	turn_side(t_turn turn)
 	*(turn.up[0]) = *(turn.left[2]);
 	*(turn.left[2]) = swap;
 
-	(void)swap;
-	(void)turn;
 	return;
 }
